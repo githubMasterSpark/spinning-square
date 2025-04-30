6763aa11d457a17d626d4095e4e1719b6a54d75d
githubMasterSpark <bdwang@ucsd.edu>
2025-04-30 14:36:57 -0700

    navbar

---
+++ src/app/components/navbar.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Spinning Square</h1>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Pineapple</Link>
+        </nav>
+    );
+}
