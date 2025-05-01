81cf8c95ee1afd0555bdc11e52cba9e592c9974c
githubMasterSpark <bdwang@ucsd.edu>
2025-05-01 12:47:18 -0700

    please work css

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
     <div>
       <h1>Home Page!</h1>
       <p>This is a home page. There is not much formatting.</p>  
-        <linebreak></linebreak>
       <p>The spinning square is not on the page, but is instead in your mind.</p>
     </div>
   );
