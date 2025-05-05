919e8ca9c7cd79b14569718e7865597329652208
githubMasterSpark <bdwang@ucsd.edu>
2025-05-05 14:52:24 -0700

    3

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1 className={styles.main}>Home Page!</h1>
+    <div>
+      <h1>Home Page!</h1>
       <p>This is a home page. There is not much formatting.</p>  
       <p>The spinning square is not on the page, but is instead in your mind.</p>
     </div>
