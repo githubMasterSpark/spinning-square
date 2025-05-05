0c4a3b4d5816e9da3ee9dd8e017c45e9acd8c373
githubMasterSpark <bdwang@ucsd.edu>
2025-05-05 14:46:34 -0700

    update 2

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page!</h1>
+    <div className={styles.page}>
+      <h1 className={styles.main}>Home Page!</h1>
       <p>This is a home page. There is not much formatting.</p>  
       <p>The spinning square is not on the page, but is instead in your mind.</p>
     </div>
